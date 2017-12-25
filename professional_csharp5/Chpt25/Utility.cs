d1749cd61025c87880435ba9a64159774aaa0ddd
binly.z <binlyzhuo@outlook.com>
2017-12-25 21:20:49 +0800

    ~

---
+++ professional_csharp5/Chpt25/Utility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Chpt25
+{
+    public static class Utility
+    {
+        public static void DisplayTransactionInfo(string title, TransactionInformation ti)
+        {
+            Contract.Requires<ArgumentNullException>(ti != null);
+
+            Console.WriteLine(title);
+            Console.WriteLine("CreateTime:{0}",ti.CreationTime);
+            Console.WriteLine("Local ID:{0}",ti.LocalIdentifier);
+            Console.WriteLine("Distributed ID:{0}",ti.DistributedIdentifier);
+        }
+    }
+}
