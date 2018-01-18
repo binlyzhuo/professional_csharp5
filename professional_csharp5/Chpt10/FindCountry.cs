3f5e961c2ea913e4e510455eaf9409319f84a4a1
binly.z <binlyzhuo@outlook.com>
2018-01-18 21:38:33 +0800

    ~

---
+++ professional_csharp5/Chpt10/FindCountry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chpt10
+{
+    public class FindCountry
+    {
+        private string country;
+        public FindCountry(string country)
+        {
+            this.country = country;
+        }
+
+        public bool FindCountryPredicate(Racer racer)
+        {
+            return racer.Country == country;
+        }
+    }
+}
