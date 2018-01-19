71afa13911ec12bf1e8f9d544535dafac13a41c6
binly.z <binlyzhuo@outlook.com>
2018-01-19 20:34:20 +0800

    ~

---
+++ professional_csharp5/Chpt10/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chpt10
+{
+    public class Document
+    {
+        public string Title { set; get; }
+        public string Content { set; get; }
+
+        public Document(string title, string content)
+        {
+            this.Title = title;
+            this.Content = content;
+        }
+    }
+}
