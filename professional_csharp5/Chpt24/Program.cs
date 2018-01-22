2ac89fa4206d5cc45bf05102445beb1d9ef37886
binly.z <binlyzhuo@outlook.com>
2018-01-22 21:56:09 +0800

    ~

---
+++ professional_csharp5/Chpt24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chpt24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = File.Open(@"d:\1234.txt",FileMode.OpenOrCreate))
+            {
+
+                fs.WriteByte(65);
+            }
+        }
+    }
+}
